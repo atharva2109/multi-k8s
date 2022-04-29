bd11e3a21e3dbdbad044086036e955846e37d165
Atharva <atharvk2109@gmail.com>
2022-04-29 10:06:42 +0530

    updated header

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Fibonnaci Series
+            Fibonnaci Series version Kubernettes
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
